dace51f68d3fe12823a65089b07ad1c864055635
sudheer52 <varma4147@gmail.com>
2017-01-02 11:37:52 +0530

    tomcat chef exp files

---
+++ cookbooks/tomcat_chef/README.md
@@ -0,0 +1,4 @@
+# tomcat_chef
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/tomcat_chef/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: tomcat_chef
+# Recipe:: default
+#
+# Copyright:: 2016, The Authors, All Rights Reserved.
