4c8fc91a9066e84885e7c6ea539b8ce716209e75
sudheer52 <varma4147@gmail.com>
2017-01-02 11:34:16 +0530

    java document

---
+++ cookbooks/java_chef/README.md
@@ -0,0 +1,4 @@
+# java_chef
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/java_chef/recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: java_chef
+# Recipe:: default
+#
+# Copyright:: 2016, The Authors, All Rights Reserved.
+include_recipe 'java'
